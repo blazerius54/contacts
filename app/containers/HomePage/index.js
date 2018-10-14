12a8e57a9845c0b62a3bd6f20f2997c58e4d9723
blazerius54 <79137837220@yandex.ru>
2018-10-14 14:14:15 +0700

    task-01/request initial funcs for request users

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,13 +13,72 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+let users = [];
+
+function preparedFetch(opts, sessionToken) {
+  const reqOpts = {
+    method: opts.method,
+    headers: {},
+  };
+
+  if (sessionToken) {
+    reqOpts.headers.Authorization = `Bearer ${sessionToken}`;
+  }
+
+  if (
+    opts.method === 'POST' ||
+    opts.method === 'PUT' ||
+    opts.method === 'PATCH' ||
+    opts.method === 'DELETE'
+  ) {
+    reqOpts.headers.Accept = 'application/json';
+    reqOpts.headers['Content-Type'] = 'application/json';
+  }
+
+  if (opts.body) {
+    reqOpts.body = opts.body;
+
+    if (
+      opts.method === 'POST' ||
+      opts.method === 'PUT' ||
+      opts.method === 'PATCH' ||
+      opts.method === 'DELETE'
+    ) {
+      reqOpts.body = JSON.stringify(opts.body);
+    }
+  }
+
+  const url = `http://demo.sibers.com/users`;
+  return fetch(url, reqOpts).then(response => {
+    if (response.status === 401) {
+      return;
+    }
+    return response;
+  });
+}
+
+function userRequest() {
+  return preparedFetch({
+    method: 'GET',
+  })
+    .then(response => {
+      if (response.status === 200) {
+        return response;
+      }
+    })
+    .then(data => data.json().then(data => (users = data)));
+}
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <button onClick={() => userRequest()}>click</button>
+        <button onClick={() => console.log(users)}>showusers</button>
+      </div>
     );
   }
 }
