1033492267a080cd9a0f1ebb5728b3a5bc040cc2
blazerius54 <79137837220@yandex.ru>
2018-10-15 22:11:59 +0700

    task-02/markup some contact info

--- app/containers/ActiveContact/index.js
+++ app/containers/ActiveContact/index.js
@@ -7,26 +7,48 @@ const ActiveContactSection = styled.div`
   flex-grow: 1;
   justify-content: center;
   align-items: flex-start;
+  margin-top: 150px;
 `;
 
 const ActiveContactWrapper = styled.div`
   display: flex;
-
+  flex-direction: column;
+  width: 30%;
   img {
     align-self: center;
   }
 `;
 
+const ContactRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 /* eslint-disable react/prefer-stateless-function */
 export default class ActiveContact extends React.PureComponent {
   render() {
-    const { activeContact } = this.props;
+    const { avatar, name, email, phone, website } = this.props.activeContact;
     return (
       <ActiveContactSection>
         <ActiveContactWrapper>
-          <img src={activeContact.avatar} alt='avatar' />
-          <p>{activeContact && activeContact.name}</p>
-        </ActiveContactWrapper>        
+          <ContactRow>
+            <img src={avatar} alt='avatar' />
+            <p>{this.props.activeContact && name}</p>
+          </ContactRow>
+          <ContactRow>
+            <p>Email:</p>
+            <p>{email}</p>
+          </ContactRow>
+          <ContactRow>
+            <p>Phone:</p>
+            <p>{phone}</p>
+          </ContactRow>
+          <ContactRow>
+            <p>Website:</p>
+            <p><a href={website}>{website}</a></p>
+          </ContactRow>
+        </ActiveContactWrapper>
       </ActiveContactSection>
     );
   }
